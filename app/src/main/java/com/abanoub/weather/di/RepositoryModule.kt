47a30796a68ebba9d8fb1f4c3719bfd988d86839
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-02-22 03:31:02 +0200

    Create Hilt RepositoryModule

---
+++ app/src/main/java/com/abanoub/weather/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.abanoub.weather.di
+
+import com.abanoub.weather.data.repository.WeatherRepositoryImpl
+import com.abanoub.weather.domain.repository.WeatherRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(
+        weatherRepositoryImpl: WeatherRepositoryImpl
+    ): WeatherRepository
+}
