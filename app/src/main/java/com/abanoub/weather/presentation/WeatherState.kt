172dd847e2ecf3680a515559e6c312f8e268cb12
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-02-22 03:29:29 +0200

    Create WeatherState Class

---
+++ app/src/main/java/com/abanoub/weather/presentation/WeatherState.kt
@@ -0,0 +1,9 @@
+package com.abanoub.weather.presentation
+
+import com.abanoub.weather.domain.weather.WeatherInfo
+
+data class WeatherState(
+    val weatherInfo: WeatherInfo? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
